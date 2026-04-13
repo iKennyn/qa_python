19a6bc46af2920ab8a374c441dd8ea8db170b276
rsharkov <r-sharkov@mail.ru>
2026-04-13 10:26:43 +0300

    Исправил проверку test_get_books_genre_success. Обновил на сравнение словаря

--- conftest.py
+++ conftest.py
@@ -14,3 +14,4 @@ def create_books_and_genre(books_collector):
     for book_name, genre in zip(data.BOOKS_COLLECTOR, data.GENRES_COLLECTOR):
         books_collector.add_new_book(book_name)
         books_collector.set_book_genre(book_name, genre)
+    return books_collector.get_books_genre()
--- tests.py
+++ tests.py
@@ -65,7 +65,8 @@ class TestBooksCollector:
         assert (book_name in books_collector.get_list_of_favorites_books()) == False
 
     def test_get_books_genre_success(self, books_collector, create_books_and_genre):
-        assert books_collector.get_books_genre() is not None
+        expected_dict = create_books_and_genre
+        assert books_collector.get_books_genre() == expected_dict
 
     def test_get_list_of_favorites_books_success(self, books_collector, create_books_and_genre):
         book_name = data.BOOK_FANTASY
